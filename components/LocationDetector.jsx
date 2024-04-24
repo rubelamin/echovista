607655dcbf25b129144c1b8b097f85b856b57d66
Rubel Amin <bagha.vai100@gmail.com>
2024-04-24 16:31:25 +0600

    Network loading image added

--- components/LocationDetector.jsx
+++ components/LocationDetector.jsx
@@ -1,7 +1,12 @@
 "use client";
 
+import Image from "next/image";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
+
+const Detecting = () => {
+	return <Image src="/network.gif" width={200} height={200} alt="Network" />;
+};
 
 export default function LocationDetector() {
 	const [loading, setLoading] = useState(false);
@@ -23,5 +28,22 @@ export default function LocationDetector() {
 		}
 	}, [pathname, router, searchParams]);
 
-	return <div>{loading && <div>Location detecting...</div>}</div>;
+	return (
+		<div className="flex justify-center items-center min-h-screen">
+			{loading && (
+				<Suspense fallback={<Detecting />}>
+					<div>
+						<Image
+							src="/network.gif"
+							width={500}
+							height={500}
+							alt="Loading.."
+							className="border rounded-md my-4"
+						/>
+						<p>Loction Detecting.....</p>
+					</div>
+				</Suspense>
+			)}
+		</div>
+	);
 }
